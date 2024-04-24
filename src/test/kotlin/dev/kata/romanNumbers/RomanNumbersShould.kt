8e1143c3bb485f29b39ffa2dd1bd512bf9e04a9c
Dagaet <alojera15@gmail.com>
2024-04-24 14:29:55 +0100

    Test(Red): RomanNumbersShould not contain more than three symbols of base 1

---
+++ src/test/kotlin/dev/kata/romanNumbers/RomanNumbersShould.kt
@@ -0,0 +1,23 @@
+package dev.kata.romanNumbers
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+// Roman Numbers
+// Should not contain more than 3 symbols of base 1
+// Should not repeat numbers of base 2
+// Should not subtract if they are from a superior order
+// Should sum all the numbers passed
+
+internal class RomanNumbersShould {
+    @Test
+    fun `Should not contain more than three symbols of base 1`() {
+        val romanNumbers = RomanNumbers()
+        val result = romanNumbers.romanNumberCreator(3)
+        val result2 = romanNumbers.romanNumberCreator(30)
+        val result3 = romanNumbers.romanNumberCreator(300)
+        assertThat(result3).isEqualTo("CCC")
+        assertThat(result2).isEqualTo("XXX")
+        assertThat(result).isEqualTo("III")
+    }
+}
