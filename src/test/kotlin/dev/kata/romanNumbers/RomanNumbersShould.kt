1d5a95c891b2f2158cbbe58fb511e48e5892fa3a
Dagaet <alojera15@gmail.com>
2024-04-29 11:18:14 +0100

    Test(Red): RomanNumbersShould generate a basic given number

--- src/test/kotlin/dev/kata/romanNumbers/RomanNumbersShould.kt
+++ src/test/kotlin/dev/kata/romanNumbers/RomanNumbersShould.kt
@@ -11,43 +11,13 @@ import org.junit.jupiter.api.Test
 
 internal class RomanNumbersShould {
     @Test
-    fun `Not contain more than three symbols of base 1`() {
+    fun `Generate a basic given number`() {
         val romanNumbers = RomanNumbers()
-        val result = romanNumbers.romanNumberCreator(3)
-        val result2 = romanNumbers.romanNumberCreator(30)
-        val result3 = romanNumbers.romanNumberCreator(300)
-        assertThat(result3).isEqualTo("CCC")
-        assertThat(result2).isEqualTo("XXX")
-        assertThat(result).isEqualTo("III")
-    }
-    @Test
-    fun `Set nines like IX`() {
-        val romanNumbers = RomanNumbers()
-        val result = romanNumbers.romanNumberCreator(9)
-        val result2 = romanNumbers.romanNumberCreator(39)
-        val result3 = romanNumbers.romanNumberCreator(309)
-        assertThat(result3).isEqualTo("CCCIX")
-        assertThat(result2).isEqualTo("XXXIX")
-        assertThat(result).isEqualTo("IX")
-    }
-    @Test
-    fun `Set fours like IV`() {
-        val romanNumbers = RomanNumbers()
-        val result = romanNumbers.romanNumberCreator(4)
-        val result2 = romanNumbers.romanNumberCreator(44)
-        val result3 = romanNumbers.romanNumberCreator(444)
-        assertThat(result3).isEqualTo("CDXLIV")
-        assertThat(result2).isEqualTo("XLIV")
-        assertThat(result).isEqualTo("IV")
-    }
-    @Test
-    fun `Not contain more than three symbols of base 5`() {
-        val romanNumbers = RomanNumbers()
-        val result = romanNumbers.romanNumberCreator(5)
-        val result2 = romanNumbers.romanNumberCreator(50)
-        val result3 = romanNumbers.romanNumberCreator(500)
-        assertThat(result3).isEqualTo("D")
-        assertThat(result2).isEqualTo("L")
-        assertThat(result).isEqualTo("V")
+        val result = romanNumbers.romanNumberCreator(1)
+        val result2 = romanNumbers.romanNumberCreator(3)
+        val result3 = romanNumbers.romanNumberCreator(5)
+        assertThat(result).isEqualTo("I")
+        assertThat(result2).isEqualTo("III")
+        assertThat(result3).isEqualTo("V")
     }
 }
