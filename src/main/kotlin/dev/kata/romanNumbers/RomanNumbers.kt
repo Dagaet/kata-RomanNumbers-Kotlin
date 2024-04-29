a63c64bb5528cd75f991846b4dfc5e5540fcd658
Dagaet <alojera15@gmail.com>
2024-04-29 11:18:20 +0100

    Test(Green): RomanNumbersShould generate a basic given number

--- src/main/kotlin/dev/kata/romanNumbers/RomanNumbers.kt
+++ src/main/kotlin/dev/kata/romanNumbers/RomanNumbers.kt
@@ -1,48 +1,25 @@
 package dev.kata.romanNumbers
 
 class RomanNumbers {
-    fun romanNumberCreator(numeriten: Int): String {
-        var output: String = ""
-        var numberTransform = numeriten
-        for (i in numberTransform downTo 0){
-            if (output.count{it == 'M'} < 3 && numberTransform >= 1000){
-                output += "M"
-                numberTransform -= 1000
-            } else if (output.count{it == 'D'} == 0 && numberTransform >= 500){
-                output += "D"
-                numberTransform -= 500
-            } else if (numberTransform >= 400){
-                output += "CD"
-                numberTransform -= 400
-            } else if (output.count{it == 'C'} < 3 && numberTransform >= 100){
-                output += "C"
-                numberTransform -= 100
-            }else if (numberTransform in 91..99){
-                output += "XC"
-                numberTransform -= 90
-            } else if (output.count{it == 'L'} == 0 && numberTransform >= 50){
-                output += "L"
-                numberTransform -= 50
-            } else if (numberTransform >= 40){
-                output += "XL"
-                numberTransform -= 40
-            } else if (output.count{it == 'X'} < 3 && numberTransform >= 10){
-                output += "X"
-                numberTransform -= 10
-            }else if (numberTransform == 9){
-                output += "IX"
-                numberTransform -= 9
-            } else if (output.count{it == 'V'} == 0 && numberTransform >= 5){
-                output += "V"
-                numberTransform -= 5
-            } else if (numberTransform == 4){
-                output += "IV"
-                numberTransform -= 4
-            } else if (output.count{it == 'I'} < 3 && numberTransform >= 1){
-                output += "I"
-                numberTransform--
+    fun romanNumberCreator(arabicValue: Int): String {
+        var romanValue = ""
+        var arabicValueTransform: Int = arabicValue
+        RomanNumbersEnum.entries.forEach { romanNumbers ->
+            while (arabicValueTransform >= romanNumbers.decimalValue) {
+                arabicValueTransform -= romanNumbers.decimalValue
+                romanValue += romanNumbers.romanValue
             }
         }
-        return output
+        return romanValue
     }
+}
+
+
+enum class RomanNumbersEnum(val romanValue: Char, val decimalValue: Int) {
+    ONETHOUSAND('M',1000),
+    FIVEHUNDRED('D', 500),
+    ONEHUNDRED('C', 100),
+    FIFTY('L', 50),
+    FIVE('V', 5),
+    ONE('I', 1)
 }
